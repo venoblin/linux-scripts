f7c29dd4191aa760e8f1327acb34e2d18dca67bc
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-20 21:17:05 -0400

    updated files

--- ezshc.sh
+++ ezshc.sh
@@ -2,5 +2,6 @@
 #Turns all .sh files to binary then removes extension
 #!/bin/bash
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
-    echo "Processing file: $file"
+    echo $file
 done
+
