dea7c82982a5b004b746e01641e49165e05007a9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-20 21:43:06 -0400

    finished ezshc

--- ezshc.sh
+++ ezshc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Turns all .sh files to binary then removes extension
 #!/bin/bash
-echo "Turning files into binaries..."
+echo "Turning files to binary"
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
     shc -f $file
 done
 
-echo "Removing extensions..."
+echo "Removing extensions"
 find . -type f -name "*.sh.x" -print0 | while IFS= read -r -d '' file; do
-    echo ${file%.sh.x}
+    mv $file ${file%.sh.x}
 done
 
 echo "Done!"
