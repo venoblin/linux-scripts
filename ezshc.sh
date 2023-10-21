b7dbd0256d69221bd289694a907282280e490d60
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-20 21:41:02 -0400

    worked on removing extensions

--- ezshc.sh
+++ ezshc.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #Turns all .sh files to binary then removes extension
 #!/bin/bash
+echo "Turning files into binaries..."
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
-    echo $file
+    shc -f $file
 done
+
+echo "Removing extensions..."
+find . -type f -name "*.sh.x" -print0 | while IFS= read -r -d '' file; do
+    echo ${file%.sh.x}
+done
+
+echo "Done!"
+
 
