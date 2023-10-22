7663b25f288890bcbbf869f077a3be6300fd329a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-21 23:28:27 -0400

    added intermediate file removal feature

--- ezshc.sh
+++ ezshc.sh
@@ -1,14 +1,20 @@
 #!/bin/zsh
-#turns all .sh files to binary then removes extension
+#turns all scripts to binary, renames them, moves them to bin directory,
+#and removes intermediate files
 echo "Turning files to binary"
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
     shc -f $file
 done
 
-echo "Removing extensions and moving to bin directory"
+echo "Renaming binaries and moving to bin directory"
 find . -type f -name "*.sh.x" -print0 | while IFS= read -r -d '' file; do
     mv $file ${file%.sh.x}
     mv ${file%.sh.x} bin/
+done
+
+echo "Cleaning up"
+find . -type f -name "*.sh.x.c" -print0 | while IFS= read -r -d '' file; do
+    rm $file
 done
 
 echo "Done"
