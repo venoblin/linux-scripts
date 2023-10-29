083b1bf3529d3404d7c97727775240d1e8dcfb77
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-29 19:18:56 -0400

    added some echos

--- ezshc.sh
+++ ezshc.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 #turns all scripts to binary, renames them, moves them to bin directory,
 #and removes intermediate files
-echo "Turning files to binary and moving them to bin directory"
+echo "Creating binaries and moving them to bin directory"
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
     if [[ $file != "./install.sh" ]]; then
         shc -f $file -o ${file%.sh}
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 if [ -d "bin" ]; then
   echo "Bin directory already exists"
 else
+  echo "Installing..."
   mkdir bin
   ./ezshc.sh
 fi
