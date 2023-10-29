c89fdf04a680d5615541a741c0691d820b9418bc
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-29 19:32:47 -0400

    modified update commmands

--- ezupdate.sh
+++ ezupdate.sh
@@ -2,11 +2,9 @@
 #zypper refresh repos and update system
 
 if which zypper &>/dev/null; then
-  sudo zypper ref
   sudo zypper update -y 
 elif which dnf &>/dev/null; then
-  sudo dnf check-update
-  sudo dnf update
+  sudo dnf update -y
 else
   echo "Unsupported package manager"
 fi
