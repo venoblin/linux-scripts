4637e85367c83e741fc79ab1e6ea39fea16384fd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-29 19:30:51 -0400

    added zypper and dnf support

--- ezupdate.sh
+++ ezupdate.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 #zypper refresh repos and update system
 
-sudo zypper ref
-sudo zypper update -y 
+if which zypper &>/dev/null; then
+  sudo zypper ref
+  sudo zypper update -y 
+elif which dnf &>/dev/null; then
+  sudo dnf check-update
+  sudo dnf update
+else
+  echo "Unsupported package manager"
+fi
