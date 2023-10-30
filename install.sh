f909a277af026a8ad46e38afb2958fb00b4b3e1e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2023-10-29 20:46:02 -0400

    modified isntall script

--- install.sh
+++ install.sh
@@ -4,7 +4,18 @@ if [ -d "bin/" ]; then
   echo "Bin directory already exists"
 else
   echo "Installing..."
+  
   echo "Creating bin directory"
   mkdir bin
+
+  echo "Writing to zshrc file"
+  bin_location="$(pwd)/bin"
+  zshrc_file="$HOME/.zshrc"
+  
+  if ! grep -q "export PATH.*$bin_location" $zshrc_file; then
+    echo 'export PATH="$PATH:'"$bin_location"'"' | cat - "$zshrc_file" > temp && mv temp "$zshrc_file"
+  fi
+  
   ./ezshc.sh
 fi
+
