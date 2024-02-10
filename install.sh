a4af9c2a7fd694cd8a65783320eb6404901205e7
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-02-09 23:48:46 -0500

    modified install script

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/zsh
 
 if [ -d "bin/" ]; then
-  echo "Bin directory already exists"
-else
-  echo "Installing..."
-  
-  echo "Creating bin directory"
-  mkdir bin
-
-  echo "Writing to zshrc file"
-  bin_location="$(pwd)/bin"
-  zshrc_file="$HOME/.zshrc"
-  if ! grep -q "export PATH.*$bin_location" $zshrc_file; then
-    echo 'export PATH="'"$bin_location"':$PATH"' | cat - "$zshrc_file" > temp && mv temp "$zshrc_file"
-  fi
-  
-  ./ezshc.sh
+  rm -rf bin/
 fi
+
+echo "Installing..."
+
+echo "Creating bin directory"
+mkdir bin
+
+echo "Writing to zshrc file"
+bin_location="$(pwd)/bin"
+zshrc_file="$HOME/.zshrc"
+if ! grep -q "export PATH.*$bin_location" $zshrc_file; then
+  echo 'export PATH="'"$bin_location"':$PATH"' | cat - "$zshrc_file" > temp && mv temp "$zshrc_file"
+fi
+
+./ezshc.sh
+
 
