a41a3580eb09866acd7849a53d85a071f5929495
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-09 23:11:14 -0800

    worked on scripts

--- helpers/getpackagemanager.sh
+++ helpers/getpackagemanager.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
 #gets systems package manager 
+
+if which zypper &>/dev/null; then
+  echo "zypper" 
+elif which dnf &>/dev/null; then
+  echo "dnf"
+elif which apt &>/dev/null; then
+  echo "apt"
+else
+  echo "Error: Unsupported package manager"
+  exit 1
+fi
--- source/ezupdate.sh
+++ source/ezupdate.sh
@@ -5,6 +5,9 @@ if which zypper &>/dev/null; then
   sudo zypper update -y 
 elif which dnf &>/dev/null; then
   sudo dnf update -y
+elif which apt &>/dev/null; then
+  sudo apt update
+  sudo apt upgrade -y
 else
   echo "Unsupported package manager"
 fi
