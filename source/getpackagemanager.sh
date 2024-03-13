77a3aafec8e206a0723305f7438d5e2d4ca45f1a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-12 22:06:33 -0400

    moved export keyword

--- source/getpackagemanager.sh
+++ source/getpackagemanager.sh
@@ -2,14 +2,12 @@
 #gets systems package manager 
 
 if which zypper &>/dev/null; then
-  PACKAGE_MANAGER="zypper" 
+  export PACKAGE_MANAGER="zypper" 
 elif which dnf &>/dev/null; then
-  PACKAGE_MANAGER="dnf"
+  export PACKAGE_MANAGER="dnf"
 elif which apt &>/dev/null; then
-  PACKAGE_MANAGER="apt"
+  export PACKAGE_MANAGER="apt"
 else
   echo "Error: Unsupported package manager"
   exit 1
 fi
-
-export $PACKAGE_MANAGER
