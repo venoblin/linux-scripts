450a30b83e42eec0f2bad012739b7c64f1df1873
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-12 22:10:46 -0400

    moved export again

--- source/getpackagemanager.sh
+++ source/getpackagemanager.sh
@@ -2,12 +2,14 @@
 #gets systems package manager 
 
 if which zypper &>/dev/null; then
-  export PACKAGE_MANAGER="zypper" 
+  PACKAGE_MANAGER="zypper" 
 elif which dnf &>/dev/null; then
-  export PACKAGE_MANAGER="dnf"
+  PACKAGE_MANAGER="dnf"
 elif which apt &>/dev/null; then
-  export PACKAGE_MANAGER="apt"
+  PACKAGE_MANAGER="apt"
 else
   echo "Error: Unsupported package manager"
   exit 1
 fi
+
+export $PACKAGE_MANAGER
