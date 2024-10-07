78989155776bfbcd5d1d78c9dfdb886b66324456
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-07 08:41:08 -0400

    worked on ezgitsetup

--- src/ezgitsetup.sh
+++ src/ezgitsetup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #easy git setup on machine, creates an ssh key and returns it 
+
+git config --global user.name "Your Name"
+git config --global user.email "yourname@example.com"
+
+git config --global init.defaultBranch main
+git config --global color.ui auto
+git config --global pull.rebase false
+
+ssh-keygen -t ed25519
+cat ~/.ssh/id_ed25519.pub
