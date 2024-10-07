3dcf6e5cd57b8c87e092b164b95d24a282b71d4d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-07 11:43:19 -0400

    removed ezgit setup from compiling

--- src/ezshc.sh
+++ src/ezshc.sh
@@ -4,7 +4,7 @@
 
 echo "Making binaries and moving them to bin directory."
 find . -type f -name "*.sh" -print0 | while IFS= read -r -d '' file; do
-    if [[ $file != "./install.sh" ]]; then
+    if [[ $file != "./install.sh" && $file != "./src/ezgitsetup.sh" ]]; then
         shc -f $file -o ${file%.sh}
         mv ${file%.sh} bin/
     fi
