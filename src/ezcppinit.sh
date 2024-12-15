e0c30070279ae83ad84af8cf0d41031ea943245e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-15 16:04:20 -0500

    finished script

--- src/ezcppinit.sh
+++ src/ezcppinit.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 #initializes a cpp project
 
+project_name="$1"
+if [[ -z "$1" ]]; then
+  echo "Error: project name needed." >&2
+  exit 1
+fi
+
 mkdir $(pwd)/src
 
 touch $(pwd)/CMakeLists.txt
-echo 'cmake_minimum_required (VERSION 3.30.5)
+echo "cmake_minimum_required (VERSION 3.30.5)
 file (GLOB_RECURSE SOURCE_FILES "src/*.cpp")
 
 project (
-  projectname
+  "$project_name"
   VERSION "0.1.0"
   DESCRIPTION "description"
   LANGUAGES CXX
 )
 
-add_executable (projectname ${SOURCE_FILES})'> $(pwd)/CMakeLists.txt
+add_executable ("$project_name" \${SOURCE_FILES})"> $(pwd)/CMakeLists.txt
 
 touch $(pwd)/build.sh
-echo '#!/bin/bash
+echo "#!/bin/bash
 
 if ! [ -d "build" ]; then
   mkdir build
@@ -25,69 +31,49 @@ fi
 
 cd build/ && cmake ../ && make
 
-echo "Built engine in build directory."' > $(pwd)/build.sh
+echo "Built "$project_name" in build directory."" > $(pwd)/build.sh
 
 touch $(pwd)/run.sh
-echo '#!/bin/bash
+echo "#!/bin/bash
 
 if [ -d "build" ]; then
-  ./build/EmberEngine
+  ./build/"$project_name"
 else
-  echo "No build found! Run build.sh first."
-fi ' > $(pwd)/run.sh
+  echo "Error: No build found! Run build.sh first."
+fi" > $(pwd)/run.sh
 
+
+ignore='.vscode/
+*.DS_STORE
+.ycm_extra_conf.*
+CMakeCache.txt
+CMakeFiles/
+CMakeScripts/
+*.cmake
+build/
+xcode_build/
+*.swp
+obj/
+*.a
+*.o
+*.data
+tags
+tmp/
+Makefile
+.clangd/
+compile_commands.json
+.cache/
+Ogre.log'
 if [ ! -f ".gitignore" ]; then
   touch $(pwd)/.gitignore
 
-  echo 'build/
-  .vscode/
-  *.DS_STORE
-  .ycm_extra_conf.*
-  CMakeCache.txt
-  CMakeFiles/
-  CMakeScripts/
-  *.cmake
-  build/
-  xcode_build/
-  *.swp
-  obj/
-  *.a
-  *.o
-  *.data
-  tags
-  tmp/
-  Makefile
-  .clangd/
-  compile_commands.json
-  .cache/
-  Ogre.log' > $(pwd)/.gitignore
+  echo -e "$ignore" > $(pwd)/.gitignore
 else
-  echo 'build/
-  .vscode/
-  *.DS_STORE
-  .ycm_extra_conf.*
-  CMakeCache.txt
-  CMakeFiles/
-  CMakeScripts/
-  *.cmake
-  build/
-  xcode_build/
-  *.swp
-  obj/
-  *.a
-  *.o
-  *.data
-  tags
-  tmp/
-  Makefile
-  .clangd/
-  compile_commands.json
-  .cache/
-  Ogre.log' >> $(pwd)/.gitignore
+  echo -e "$ignore" >> $(pwd)/.gitignore
 fi 
 
 if [ ! -f "README.md" ]; then
   touch $(pwd)/.README.md
 
-  echo '# Project name' > $(pwd)/.README.md
+  echo "# "$project_name"" > $(pwd)/.README.md
 fi 
