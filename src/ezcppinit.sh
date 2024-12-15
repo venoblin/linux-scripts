63f1f05dcf9c3345ea6211937b96b0de3583d32d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-15 15:03:28 -0500

    finished script

--- src/ezcppinit.sh
+++ src/ezcppinit.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 #initializes a cpp project
 
-mkdir $(pwd)/src
+# mkdir $(pwd)/src
 
-touch $(pwd)/CMakeLists.txt
-echo 'cmake_minimum_required (VERSION 3.30.5)
-file (GLOB_RECURSE SOURCE_FILES "src/*.cpp")
+# touch $(pwd)/CMakeLists.txt
+# echo 'cmake_minimum_required (VERSION 3.30.5)
+# file (GLOB_RECURSE SOURCE_FILES "src/*.cpp")
 
-project (
-  projectname
-  VERSION "0.1.0"
-  DESCRIPTION "description"
-  LANGUAGES CXX
-)
+# project (
+#   projectname
+#   VERSION "0.1.0"
+#   DESCRIPTION "description"
+#   LANGUAGES CXX
+# )
 
-add_executable (projectname ${SOURCE_FILES})'> $(pwd)/CMakeLists.txt
+# add_executable (projectname ${SOURCE_FILES})'> $(pwd)/CMakeLists.txt
 
-touch $(pwd)/build.sh
-echo '#!/bin/bash
+# touch $(pwd)/build.sh
+# echo '#!/bin/bash
 
-if ! [ -d "build" ]; then
-  mkdir build
-fi
+# if ! [ -d "build" ]; then
+#   mkdir build
+# fi
 
-cd build/ && cmake ../ && make
+# cd build/ && cmake ../ && make
 
-echo "Built engine in build directory."' > $(pwd)/build.sh
+# echo "Built engine in build directory."' > $(pwd)/build.sh
 
-touch $(pwd)/run.sh
-echo '#!/bin/bash
+# touch $(pwd)/run.sh
+# echo '#!/bin/bash
 
-if [ -d "build" ]; then
-  ./build/EmberEngine
-else
-  echo "No build found! Run build.sh first."
-fi ' > $(pwd)/run.sh
+# if [ -d "build" ]; then
+#   ./build/EmberEngine
+# else
+#   echo "No build found! Run build.sh first."
+# fi ' > $(pwd)/run.sh
+
+if [ ! -f ".gitignore" ]; then
+  echo "no git ignore"
+fi 
