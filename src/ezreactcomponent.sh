e21164338a2d28491abe01577ba7fc106878aab1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-15 23:22:59 -0500

    added echo command to fill in jsx file

--- src/ezreactcomponent.sh
+++ src/ezreactcomponent.sh
@@ -3,6 +3,17 @@
 
 mkdir $(pwd)/$1
 touch $(pwd)/$1.jsx
+echo "import './"$1".css'
+
+const "$1" = () => {
+  return (
+    <div className='"$1"'>
+
+    </div>
+  )
+}
+
+export default "$1"" > $(pwd)/$1.jsx
 touch $(pwd)/$1.scss
 
 mv $(pwd)/$1.jsx $(pwd)/$1
