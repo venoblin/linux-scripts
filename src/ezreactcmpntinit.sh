21232f14ae18a433ddd3555ab8ebe94bcebdb886
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-17 21:30:28 -0500

    worked on react init script

--- src/ezreactcmpntinit.sh
+++ src/ezreactcmpntinit.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 #creates a directory of a certain name with a jsx file and scss file with the same name
 
-mkdir $(pwd)/$1
-touch $(pwd)/$1.jsx
+type="jsx"
+
+if [[ ! "$1" ]]; then
+  echo "Error: Path needed!" >&2
+  exit 1
+fi
+
+if [[ "$2" == "-j" ]]; then
+  type="jsx"
+elif [[ "$2" == "-t" ]]; then
+  type="tsx"
+fi
+
+mkdir $(pwd)/"$1"
+touch $(pwd)/"$1"."$type"
 echo "import './"$1".css'
 
 const "$1" = () => {
@@ -13,8 +26,8 @@ const "$1" = () => {
   )
 }
 
-export default "$1"" > $(pwd)/$1.jsx
-touch $(pwd)/$1.scss
+export default "$1"" > $(pwd)/$1."$type"
+touch $(pwd)/"$1".scss
 
-mv $(pwd)/$1.jsx $(pwd)/$1
-mv $(pwd)/$1.scss $(pwd)/$1
+mv $(pwd)/"$1".jsx $(pwd)/"$1"
+mv $(pwd)/"$1".scss $(pwd)/"$1"
--- src/ezreadmeinit.sh
+++ src/ezreadmeinit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #initializes readme template at specified path
 
-if [ -f "README.md" ]; then
-  echo "Error: A README.md already in directory!"
-  exit 1
-fi 
-
 scripts_location=$(dirname $(dirname $(command -v ezreadmeinit)))
 readme_file="$scripts_location/files/README.md"
+
+if [ -f "README.md" ]; then
+  echo "Error: A README.md already in directory!" >&2
+  exit 1
+fi 
 
 cp -r $readme_file $(pwd)
