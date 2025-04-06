330afa9d6f01df0649068ad23d4ae9ce64e67aa6
Jonathan <villagomezhjonathan@gmail.com>
2025-04-06 05:07:20 -0400

    uncommented update commands

--- standalone/ezpiholeupdate.sh
+++ standalone/ezpiholeupdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #updates Pi running Raspberry Pi OS, Pi-Hole, and Pi-VPN
 
-#sudo apt update
-#sudo apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
-#sudo pihole -g
-#sudo pihole up
+sudo pihole -g
+sudo pihole up
 
 path="/home/jvhmx/dev/linux-scripts"
 timestamp=$(date "+%Y-%m-%d_%H:%M:%S")
