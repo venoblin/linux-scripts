b40f6a4d6d8a5a84bc6224531444b66750c65c02
Jonathan <villagomezhjonathan@gmail.com>
2025-04-06 04:24:55 -0400

    added logging for piholeupdates

--- standalone/ezpiholeupdate.sh
+++ standalone/ezpiholeupdate.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #updates Pi running Raspberry Pi OS, Pi-Hole, and Pi-VPN
 
-sudo apt update
-sudo apt upgrade -y
+#sudo apt update
+#sudo apt upgrade -y
 
-sudo pihole -g
-sudo pihole -up
+#sudo pihole -g
+#sudo pihole up
+
+if [ ! -d "$HOME/dev/linux-scripts/logs" ]; then
+  mkdir $HOME/dev/linux-scripts/logs
+fi
+
+timestamp=$(date "+%Y-%m-%d_%H:%M:%S")
+
+touch $HOME/dev/linux-scripts/logs/update-$timestamp.txt
+echo "Pi updated on $timestamp" > $HOME/dev/linux-scripts/logs/update-$timestamp.txt
