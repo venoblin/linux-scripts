823d8cc40f5dfefdfe56f4ba9eea82c44d48b54f
Jonathan <villagomezhjonathan@gmail.com>
2025-04-06 05:06:36 -0400

    fixeed script

--- standalone/ezpiholeupdate.sh
+++ standalone/ezpiholeupdate.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #updates Pi running Raspberry Pi OS, Pi-Hole, and Pi-VPN
 
-sudo apt update
-sudo apt upgrade -y
+#sudo apt update
+#sudo apt upgrade -y
 
-sudo pihole -g
-sudo pihole up
+#sudo pihole -g
+#sudo pihole up
 
 path="/home/jvhmx/dev/linux-scripts"
 timestamp=$(date "+%Y-%m-%d_%H:%M:%S")
+name="pi-hole-update"
 
-if [ ! -d "$path/dev/linux-scripts/logs" ]; then
+if [ ! -d "$path/logs" ]; then
   mkdir $path/logs
+  sudo chown jvhmx $path/logs
 fi
 
-touch $path/logs/pi-hole-update-$timestamp.txt
-echo "Pi updated on $timestamp" > $path/logs/pi-hole-update-$timestamp.txt
+touch $path/logs/$name-$timestamp.txt
+echo "Pi updated on $timestamp" > $path/logs/$name-$timestamp.txt
+sudo chown jvhmx $path/logs/$name-$timestamp.txt
