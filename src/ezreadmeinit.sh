c96e20f86c917201d97635ab5e9dd04a2183b371
Jonathan <villagomezhjonathan@gmail.com>
2026-01-04 00:52:57 -0500

    updated script

--- src/ezreadmeinit.sh
+++ src/ezreadmeinit.sh
@@ -2,8 +2,8 @@
 #initializes readme template at specified path
 
 scripts_location=$(dirname $(dirname $(command -v ezreadmeinit)))
-readme_file="$scripts_location/files/project-template/README.md"
-project_images="$scripts_location/files/project-template/.project-images"
+readme_file="$scripts_location/files/readme-template/README.md"
+readme_images="$scripts_location/files/readme-template/.readme-images"
 
 if [ -f "README.md" ]; then
   echo "Error: A README.md already in directory!" >&2
@@ -11,4 +11,4 @@ if [ -f "README.md" ]; then
 fi 
 
 cp -r "$readme_file" $(pwd)
-cp -r "$project_images" $(pwd)
+cp -r "$readme_images" $(pwd)
